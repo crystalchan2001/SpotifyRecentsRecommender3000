d4957af822cfa00974dcbb70d4c2d8ef1b01c56a
“Crystal <K19001568@kcl.ac.uk>
2024-01-15 23:33:11 +0000

    Improve seed input error check

--- createPlaylist.py
+++ createPlaylist.py
@@ -1,6 +1,5 @@
 from spotifyBrain import SpotifyBrain
 
-
 # The main file that...
 # 1. Gets recently played tracks
 # 2. Asks the user to choose x seed tracks
@@ -11,9 +10,10 @@ from spotifyBrain import SpotifyBrain
 def main():
     print("Welcome to the Spotify Recents Recommender 3000!")
     spotify_brain = SpotifyBrain()
-    # getting the last played tracks
+
     num_tracks_to_visualise = int(input("How many of your recently played tracks would you like to choose from? (Between 1 and 50): "))
 
+    # check the input is valid
     invalid_limit = (num_tracks_to_visualise <= 0 or num_tracks_to_visualise > 50)
     while invalid_limit:
         num_tracks_to_visualise = int(input("You have entered an invalid number of tracks! Please enter a number between 1 and 50: "))
@@ -28,19 +28,41 @@ def main():
     
     # choosing which tracks to use as a seed to generate a playlist
     indexes = input("Enter the tracks you want to use as seeds by track id separated by a space (Enter between 1 and 5 indexes): ")
-    indexes_list = indexes.split()
-    num_seeds = len(indexes_list)
 
-    invalid_seeds = (num_seeds <= 0 or num_seeds > 5)
+    # check inputted seeds are valid
+    invalid_seeds = True
     while invalid_seeds:
-        num_seeds = int(input("You have entered an invalid number of indexes! Please enter between 1 and 5 indexes to be used as seeds."))
-        if (num_seeds > 0 and num_seeds <= 5):
+        try:
+            indexes_list = [eval(i) for i in indexes.strip().split()]
+        except:
+            indexes = input("Please only enter indexes separated by a space: ")
+            continue
+
+        num_seeds = len(indexes_list)
+        print(num_seeds, f"[{indexes_list}]")
+
+        #* ERROR CHECK FOR invalid index value 
+        invalid_num_seeds = (num_seeds <= 0 or num_seeds > 5)
+        invalid_seed_value = not all((idx > 0 and idx <= len(last_played_tracks)) for idx in indexes_list) 
+        print(f"invalid_num_seeds = {invalid_num_seeds}. invalid_seed_value = {invalid_seed_value}. len(last_played_tracks) = {len(last_played_tracks)}")
+
+        if (invalid_num_seeds):
+            indexes = input("You have entered an invalid number of indexes! Please enter between 1 and 5 indexes to be used as seeds: ")
+            continue 
+
+        elif (invalid_seed_value):
+            indexes = input("You have entered an invalid track index! Please enter indexes from the list of tracks above: ")
+            continue
+        
+        elif (not invalid_num_seeds and not invalid_seed_value):
             invalid_seeds = False
 
     seed_tracks = [last_played_tracks[int(index)-1] for index in indexes_list]
 
     # choosing how many tracks they want recommended in the new playlist
     limit = int(input("How many tracks would you like recommended in your new playlist? (Between 1 and 100):"))
+
+    # check the input is valid
     invalid_limit = (limit < 1 or limit > 100)
     while invalid_limit:
         limit = int(input("You have entered an invalid number of tracks! Please enter a number between 1 and 100: "))
